452a10cbfb4b1490c1b3ec1c5da766706e60ade1
Levi Durfee <levi.durfee@gmail.com>
2017-07-25 22:00:13 -0400

    add comments

--- src/js/ampbin.js
+++ src/js/ampbin.js
@@ -12,26 +12,62 @@ var config = {
 };
 firebase.initializeApp(config);
 
+/**
+ * Database class
+ *
+ * Simple wrapper for some Firebase database methods.
+ */
 class Database {
+  /**
+   * Database construct to set the 'table'
+   * 
+   * @param  {String} ref The database 'table' or 'folder'
+   */
   constructor(ref) {
     this.db = firebase.database().ref(ref);
   }
 
+  /**
+   * Set some values
+   *
+   * This method will set (overwrite) values in a table.
+   * 
+   * @param  {Object} obj Object that contains data
+   */
   set(obj) {
     this.db.set(obj);
   }
 
+  /**
+   * Push some values
+   *
+   * This method will append values to the table.
+   * 
+   * @param  {Object} obj Object that contains data
+   */
   push(obj) {
     this.db.push(obj);
   }
 }
 
+/**
+ * Ampbin class
+ *
+ * Main class to handle everything.
+ */
 class Ampbin {
-  
+  /**
+   * Ampbin construct
+   * 
+   * @param  {Database} database Dependency Injection of Database class
+   */
   constructor(database) {
     this.database = database;
   }
 
+  /**
+   * Save the current bin
+   */
   save() {
     // TODO: find better way to do this
     let $editors = jotted.$container.querySelectorAll('.jotted-editor');
@@ -45,11 +81,21 @@ class Ampbin {
     this.database.push(obj);
   }
 
+  /**
+   * Listen for a click on the save button
+   * 
+   * @param {String} saveId ID of the Save button
+   */
   addSaveHandler(saveId = 'save') {
     let saveEl = this.getById(saveId);
     saveEl.onclick = () => this.save();
   }
 
+  /**
+   * Get element by ID
+   * @param  {String}  elId ID of the element
+   * @return {Element}      Element Object
+   */
   getById(elId) {
     return document.getElementById(elId);
   }

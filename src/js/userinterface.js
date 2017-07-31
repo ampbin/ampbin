800e794dc991cd808ea14e7a5e4579080918940c
Levi Durfee <levi.durfee@gmail.com>
2017-07-31 18:18:32 -0400

    [Bin Links] Get links working (#15)

--- src/js/userinterface.js
+++ src/js/userinterface.js
@@ -8,6 +8,17 @@ export class UserInterface {
     this.registerSetup();
     this.logout();
     this.login();
+    this.binLinks();
+  }
+
+  binLinks() {
+    let binlinks = document.getElementsByClassName('binlink');
+    for(let i=0; i<binlinks.length; i++) {
+      binlinks[i].onclick = (e) => {
+        window.location.href = e.srcElement.href;
+        location.reload();
+      };
+    }
   }
 
   login() {

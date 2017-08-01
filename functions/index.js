9ba0e417f8d2e5b90cc67037852c813f63008fc4
Levi Durfee <levi.durfee@gmail.com>
2017-07-31 22:05:11 -0400

    [Cloud Functions] disable for now

--- functions/index.js
+++ functions/index.js
@@ -6,19 +6,19 @@ admin.initializeApp(functions.config().firebase);
 
 // Keeps track of the length of the 'likes' child list in a separate property.
 exports.binCount = functions.database.ref('/bins/{binid}').onWrite(event => {
-  const collectionRef = event.data.ref.parent;
-  const countRef = collectionRef.parent.child('bin_count');
+  // const collectionRef = event.data.ref.parent;
+  // const countRef = collectionRef.parent.child('bin_count');
 
-  // Return the promise from countRef.transaction() so our function 
-  // waits for this async event to complete before it exits.
-  return countRef.transaction(current => {
-    if (event.data.exists() && !event.data.previous.exists()) {
-      return (current || 0) + 1;
-    }
-    else if (!event.data.exists() && event.data.previous.exists()) {
-      return (current || 0) - 1;
-    }
-  }).then(() => {
-    console.log('Counter updated.');
-  });
+  // // Return the promise from countRef.transaction() so our function 
+  // // waits for this async event to complete before it exits.
+  // return countRef.transaction(current => {
+  //   if (event.data.exists() && !event.data.previous.exists()) {
+  //     return (current || 0) + 1;
+  //   }
+  //   else if (!event.data.exists() && event.data.previous.exists()) {
+  //     return (current || 0) - 1;
+  //   }
+  // }).then(() => {
+  //   console.log('Counter updated.');
+  // });
 });

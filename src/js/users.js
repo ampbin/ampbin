a93e9dd687d2132d368f68de8a676d6261573634
Levi Durfee <levi.durfee@gmail.com>
2017-07-31 21:53:18 -0400

    [Users] Convert anonymous to registered

--- src/js/users.js
+++ src/js/users.js
@@ -2,14 +2,26 @@ export class Users {
   constructor(ampbin) {
     this.ampbin = ampbin;
     this.listen();
+    let response = firebase.auth().signInAnonymously().catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.error(errorCode);
+    });
+    response.then((data) => {
+      //console.log(data);
+    });
   }
 
   register(email, password) {
-    // register the user
-    let response = firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-    });
+      let credential = firebase.auth.EmailAuthProvider.credential(email, password);
+      let self = this;
+      firebase.auth().currentUser.linkWithCredential(credential).then(function(user) {
+        self.toggle('not-logged-in', 'none');
+        self.toggle('logged-in', 'block');
+        console.log("Account linking success", user);
+      }, function(error) {
+        console.log("Account linking error", error);
+      });
   }
 
   login(email, password) {
@@ -35,8 +47,12 @@ export class Users {
     let self = this;
     firebase.auth().onAuthStateChanged(function(user) {
       if (user) {
-        self.toggle('not-logged-in', 'none');
-        self.toggle('logged-in', 'block');
+        let isAnonymous = user.isAnonymous;
+
+        if(isAnonymous === false) {
+          self.toggle('not-logged-in', 'none');
+          self.toggle('logged-in', 'block');
+        }
         self.ampbin.loggedIn = true;
         self.ampbin.binListener(user.uid);
       }

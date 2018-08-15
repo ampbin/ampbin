fda92a0e9940b394d86a542fffd18b84e8563a8b
Levi Durfee <levi.durfee@gmail.com>
2018-08-15 18:59:42 -0400

    Add JS to copy URL to clipboard

--- src/js/index.js
+++ src/js/index.js
@@ -37,3 +37,15 @@ newbin.onclick = function() {
     updateHash('');
     editor.setValue(document.getElementById('editor').value);
 }
+
+var copyurlbutton = document.getElementById('copyurl');
+copyurlbutton.onclick = function() {
+    
+    var dummy = document.createElement('input'),
+    text = window.location.href;
+    document.body.appendChild(dummy);
+    dummy.value = text;
+    dummy.select();
+    document.execCommand('copy');
+    document.body.removeChild(dummy);
+}

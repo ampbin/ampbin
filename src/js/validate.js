69c5e3e49603206421da510852879ccf79620068
Levi Durfee <levi.durfee@gmail.com>
2018-08-15 18:27:13 -0400

    Use thumbs up/down for validate

--- src/js/validate.js
+++ src/js/validate.js
@@ -1,7 +1,15 @@
 export function updateAmpStatus(status) {
-    var ampStatus = document.getElementById('amp_status');
-    ampStatus.innerHTML = status;
-    ampStatus.classList.remove('pass');
-    ampStatus.classList.remove('fail');
-    ampStatus.classList.add(status.toLowerCase());
+    var ampStatusContainer = document.getElementById('amp_status');
+    ampStatusContainer.classList.remove('pass');
+    ampStatusContainer.classList.remove('fail');
+    ampStatusContainer.classList.add(status.toLowerCase())
+    
+    var ampStatusIcon = document.getElementById('amp_status_icon');
+    ampStatusIcon.classList.remove('fa-thumbs-up');
+    ampStatusIcon.classList.remove('fa-thumbs-down');
+    if(status === 'PASS') {
+        ampStatusIcon.classList.add('fa-thumbs-up');
+    } else {
+        ampStatusIcon.classList.add('fa-thumbs-down');
+    }
 }

59b5d6077ec88f35da3d5c6159ad86d673823fa4
Levi Durfee <levi.durfee@gmail.com>
2018-08-18 21:18:06 -0400

    Add attempt

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,11 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const storage = require('@google-cloud/storage')();
+
 admin.initializeApp(functions.config().firebase);
 
 const firestore = admin.firestore();
+
 firestore.settings({
     timestampsInSnapshots: true
 });
@@ -23,3 +26,28 @@ exports.binCounter = functions.region('us-east1').firestore.document('/bins/{doc
             console.log(err);
         });
     });
+
+// feel like there is a better way to do this.
+// exports.createFile = functions.region('us-east1').firestore.document('/bins/{documentId}')
+//     .onCreate((snap, context) => {
+//         let amphtml = snap.data().amphtml;
+//         let binid = context.params.documentId;
+//         let filename = binid + '.html';
+// 
+//         const bucket = storage.bucket('ampbin-fe479.appspot.com');
+//         const file = bucket.file(filename);
+//         return file.setMetadata({
+//             contentType: 'text/html'
+//         }).then(() => {
+//             const uploadStream = file.createWriteStream();
+//             uploadStream.on('error', (err) => {
+//                 res.send(err);
+//             }).on('finish', () => {
+//                 res.send('ok');
+//             });
+//             uploadStream.write(amphtml);
+//             uploadStream.end();
+// 
+//             return 'done';
+//         })
+// });

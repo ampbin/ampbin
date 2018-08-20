21e3f827fe10f5ef673416bee97aff05889b91c1
Levi Durfee <levi.durfee@gmail.com>
2018-08-19 20:12:50 -0400

    Fix toaster messages (#52)
    * Set innerHTML instead of appendChild to div
    
    * Tidy up
    
    * Close #51

--- src/js/toast.js
+++ src/js/toast.js
@@ -2,14 +2,11 @@ export function toast(message, level) {
     let toast = document.getElementById('toast');
     toast.classList.add(level);
     toast.classList.add('visible');
+    toast.innerHTML = message;
     
-    let txt = document.createTextNode(message);
-    
-    toast.appendChild(txt);
-    
-    setTimeout(function(){ 
+    setTimeout(() => { 
         toast.classList.remove(level);
         toast.classList.remove('visible');
-        toast.removeChild(txt);
+        toast.innerHTML = '';
     }, 3000);
 }

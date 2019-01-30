c5e58388f79e22a5c251df5bc6e0a43f4cae8379
Levi Durfee <levi.durfee@gmail.com>
2019-01-30 10:00:50 -0500

    Check if button requires auth [#62]

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -11,6 +11,9 @@ export interface ButtonIcon {
 export class Button extends Component<ButtonIcon> {
 
   render(props: ButtonIcon) {
+    if(this.props.auth == false && this.props.hide == true) {
+      return '';
+    }
     return (
       <button class="fa-container" title={props.title} onClick={props.onClick}>
         <i class={'fal ' + props.name}></i>

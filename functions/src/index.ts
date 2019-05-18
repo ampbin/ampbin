7fcf79e515ccf18ddc2c05a16cc4ee7a922874b2
Levi Durfee <levi.durfee@gmail.com>
2019-05-18 19:34:10 -0400

    Get AMP HTML from database

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,37 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 import *  as express from 'express';
+
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
 
 const app = express();
 
-app.get('/', (req, res) => {
-    console.log(req);
-    res.send(`
-      <!doctype html>
-      <head>
-        <title>Time</title>
-      </head>
-      <body>
-        <p>Hi</p>
-      </body>
-    </html>`);
+app.get('/:id', (req, res) => {
+    let id = req.params.id;
+    id = id.replace('.html', '');
+    if (!id) {
+        res.send('');
+        return;
+    }
+    const bin = db.collection('bins').doc(id);
+    bin.get().then(doc => {
+        if(!doc.exists) {
+            res.send('');
+            return;
+        }
+        if(doc.exists) {
+            const data = doc.data();
+            if(data) {
+                res.send(data.amphtml);
+                return;
+            }
+        }
+    }).catch(err => {
+        res.send('Error getting document.');
+        return;
+    })
+    ;
   });
 
 exports.app = functions.https.onRequest(app);

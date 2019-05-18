ed051d6ed0fbef49478318bc1adca52446497d8d
Levi Durfee <levi.durfee@gmail.com>
2019-05-18 18:51:55 -0400

    Get static hosting working with function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,19 @@
 import * as functions from 'firebase-functions';
+import *  as express from 'express';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log(req);
+    res.send(`
+      <!doctype html>
+      <head>
+        <title>Time</title>
+      </head>
+      <body>
+        <p>Hi</p>
+      </body>
+    </html>`);
+  });
+
+exports.app = functions.https.onRequest(app);
